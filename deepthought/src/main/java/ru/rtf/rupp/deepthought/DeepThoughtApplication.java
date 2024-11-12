1022858e246fcab598c67c3cf3c138b8a31d08cc
Vaganov Vladislav <vaganovlad@gmail.com>
2024-11-13 01:19:29 +0500

    init

---
+++ deepthought/src/main/java/ru/rtf/rupp/deepthought/DeepThoughtApplication.java
@@ -0,0 +1,13 @@
+package ru.rtf.rupp.deepthought;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeepThoughtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeepThoughtApplication.class, args);
+	}
+
+}
