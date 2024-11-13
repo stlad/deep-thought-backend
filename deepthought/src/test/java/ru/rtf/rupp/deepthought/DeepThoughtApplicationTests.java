d7eae0850513e1d4121e066130203306dc363b75
Vaganov Vladislav <82213506+stlad@users.noreply.github.com>
2024-11-13 12:46:57 +0500

    init (#2)

---
+++ deepthought/src/test/java/ru/rtf/rupp/deepthought/DeepThoughtApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.rtf.rupp.deepthought;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeepThoughtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
